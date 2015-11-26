07bf0251c9d7038017ad96c9a1f8b5893a348a96
Ted Meyer <tmathmeyer@gmail.com>
2015-11-25 23:27:16 -0500

    omg

--- src/lwbi.c
+++ src/lwbi.c
@@ -11,40 +11,42 @@
 #include <string.h>
 #include <stdio.h>
 
+#define BR_SIZE 512
+
 int get_battery_percent(char *forbattery) {
     char *path = "/sys/class/power_supply";
     FILE *fp;
-    char battery_read[512];
+    char b_read[BR_SIZE];
     char tmp[64];
 
     int energy_now = 1;
     int energy_full = 1;
 
-    snprintf(battery_read, sizeof battery_read, "%s/%s/%s", path, forbattery, "energy_now");
-    fp = fopen(battery_read, "r");
+    snprintf(b_read, BR_SIZE, "%s/%s/%s", path, forbattery, "energy_now");
+    fp = fopen(b_read, "r");
     if(fp != NULL) {
         if (fgets(tmp, sizeof tmp, fp)) energy_now = atoi(tmp);
         fclose(fp);
     } else {
-        snprintf(battery_read, sizeof battery_read, "%s/%s/%s", path, forbattery, "charge_now");
-        fp = fopen(battery_read, "r");
+        snprintf(b_read, BR_SIZE, "%s/%s/%s", path, forbattery, "charge_now");
+        fp = fopen(b_read, "r");
         if(fp != NULL) {
             if (fgets(tmp, sizeof tmp, fp)) energy_now = atoi(tmp);
             fclose(fp);
         } else {
-            perror(battery_read);
+            perror(b_read);
             return -1;
         }
     }
 
-    snprintf(battery_read, sizeof battery_read, "%s/%s/%s", path, forbattery, "energy_full");
-    fp = fopen(battery_read, "r");
+    snprintf(b_read, BR_SIZE, "%s/%s/%s", path, forbattery, "energy_full");
+    fp = fopen(b_read, "r");
     if(fp != NULL) {
         if (fgets(tmp, sizeof tmp, fp)) energy_full = atoi(tmp);
         fclose(fp);
     } else {
-        snprintf(battery_read, sizeof battery_read, "%s/%s/%s", path, forbattery, "charge_full");
-        fp = fopen(battery_read, "r");
+        snprintf(b_read, BR_SIZE, "%s/%s/%s", path, forbattery, "charge_full");
+        fp = fopen(b_read, "r");
         if(fp != NULL) {
             if (fgets(tmp, sizeof tmp, fp)) energy_full = atoi(tmp);
             fclose(fp);
